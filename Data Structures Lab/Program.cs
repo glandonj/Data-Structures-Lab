8c1c04bb69c39d81bd9ab666fbcf81e7a2162759
john <glandonj@gmail.com>
2023-02-02 15:48:39 -0500

    First commit

---
+++ Data Structures Lab/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Hello and welcome to the Word Mirror, which reverses anything you submit.");
+Console.Write("Please enter a word: ");
+string input = Console.ReadLine().Trim();
+
+Console.WriteLine(StringReverse(input));
+
+static string StringReverse(string input)
+{
+    Stack<char> myStack = new Stack<char>();
+    string reverseinput = "";
+        foreach(char w in input)
+    {
+        myStack.Push(w);
+    }
+        foreach(char c in myStack)
+    {
+        reverseinput += c;
+    }
+    return reverseinput;
+}
+
+Console.ReadLine();
